c5f51a1e59960bd87af85ab49613062e4970fe95
sunk3rn <kajakobliha@seznam.cz>
2023-03-12 16:30:15 +0100

    added comments

--- smenarna.py
+++ smenarna.py
@@ -11,10 +11,26 @@ kurzovniListekOkno = Toplevel(hlavni)
 kurzovniListekOkno.title("Kurzovní lístek")
 kurzovniListekOkno.minsize(300,100)
 
-operace = StringVar(value="koupit")
-mena = StringVar(value="USD")
-vystupText = ""
+kurzy = {
+    "USD": 22.21,
+    "EUR": 23.67,
+    "GBP": 26.73,
+    "JPY": 0.160,
+    "PXN": 1.2,
+}
 
+# Hlavni komponenta vedlejisho okna - pomoci Text()
+kurzovniListek = Text(kurzovniListekOkno,width=30,height=10) 
+kurzovniListek.pack()
+for meny,kurz in kurzy.items():
+    kurzovniListek.insert(END,f"1 {meny} = {kurz} CZK\n")
+kurzovniListek.grid(row=0,column=2,rowspan=10)
+
+operace = StringVar(value="koupit") # Promena pro vyber operace
+mena = StringVar(value="USD") # Promena pro vyber meny
+vystupText = "" # Promena pro konecny vystup
+
+# Cast 1 - Vyber operace
 napis1 = Label(hlavni,text="Vyberte akci:",font="12",)
 napis1.grid(row=0,column=0,stick=W)
 vyberKoupit = Radiobutton(hlavni,text="Koupit",variable=operace,value="koupit")
@@ -22,33 +38,21 @@ vyberProdat = Radiobutton(hlavni,text="Prodat",variable=operace,value="prodat")
 vyberKoupit.grid(row=1,column=0,sticky=W+N)
 vyberProdat.grid(row=1,column=1,sticky=E+N)
 
-kurzy = {
-    "USD": 22.21,
-    "EUR": 23.67,
-    "GBP": 26.73,
-    "JPY": 0.160,
-}
-
-kurzovniListek = Text(kurzovniListekOkno,width=30,height=10)
-kurzovniListek.pack()
-for meny,kurz in kurzy.items():
-    kurzovniListek.insert(END,f"1 {meny} = {kurz} CZK\n")
-kurzovniListek.grid(row=0,column=2,rowspan=10)
-
-
+# Cast 2 - Vyber meny
 napis2 = Label(text="Vyberte měnu:",font="12",)
 napis2.grid(row=2,column=0,sticky=W)
 
 vyberMena = OptionMenu(hlavni,mena,*kurzy)
 vyberMena.grid(row=3,column=1,sticky=N,padx=50)
 
+#Cast 3 - Zadavani vstupni castky
 napis3 = Label(hlavni,text="Zadejte částku:",font="12",)
 napis3.grid(row=4,column=0,sticky=W)
 
 vstupCastka = Entry(hlavni)
 vstupCastka.grid(row=5,column=1,padx=50)
 
-def vypocet(mena,castka,operace):
+def vypocet(mena,castka,operace): # Hlavni vypocet, vstupem je zvolena mena, zadana castka a zvolena operace
     priplatek = 0.5 / 100
     if operace == "koupit":
         vystupniCena = castka * mena * (1 - priplatek)
@@ -62,14 +66,14 @@ def vypocet(mena,castka,operace):
         pocatecniText = "Vybíraná částka je:"
         return (pocatecniText,vystupniCena,vystupniPoplatek)
 
-def aktualizaceVystupu():
+def aktualizaceVystupu(): # Aktualizuje text v Labelu vystup na 7 radku
     castka = float(vstupCastka.get())
     mena2 = kurzy.get(mena.get())
     operace2 = operace.get()
     pocatecniText,vystupniCena,vystupniPoplatek = vypocet(mena2,castka,operace2)
     vystup["text"] = "{} {:.2f} Kč, základ je:{:.2f} a poplatek: {:.2f}".format(pocatecniText,vystupniCena,castka,vystupniPoplatek)
 
-def aktualizaceHodin():
+def aktualizaceHodin(): # Aktualizuje hodiny
     hodiny["text"]= datetime.now().strftime("%H:%M:%S")
     hlavni.after(1000, aktualizaceHodin)
 
