239705dec25997eeffb7e4bfa5e9f7651280129a
sunk3rn <kajakobliha@seznam.cz>
2023-03-12 16:10:06 +0100

    added clock

--- smenarna.py
+++ smenarna.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 #Hlavni okno
 hlavni = Tk()
-hlavni.minsize(400,300)
+hlavni.minsize(500,200)
 hlavni.title("Směnárna")
 
 #Vedlejsi okno - kurzovni listek
-kurzovniListekOkno = Toplevel()
+kurzovniListekOkno = Toplevel(hlavni)
 kurzovniListekOkno.title("Kurzovní lístek")
 kurzovniListekOkno.minsize(300,100)
 
@@ -19,8 +19,8 @@ napis1 = Label(hlavni,text="Vyberte akci:",font="12",)
 napis1.grid(row=0,column=0,stick=W)
 vyberKoupit = Radiobutton(hlavni,text="Koupit",variable=operace,value="koupit")
 vyberProdat = Radiobutton(hlavni,text="Prodat",variable=operace,value="prodat")
-vyberKoupit.grid(row=1,column=0,sticky=W)
-vyberProdat.grid(row=1,column=1)
+vyberKoupit.grid(row=1,column=0,sticky=W+N)
+vyberProdat.grid(row=1,column=1,sticky=E+N)
 
 kurzy = {
     "USD": 22.21,
@@ -40,13 +40,13 @@ napis2 = Label(text="Vyberte měnu:",font="12",)
 napis2.grid(row=2,column=0,sticky=W)
 
 vyberMena = OptionMenu(hlavni,mena,*kurzy)
-vyberMena.grid(row=3,column=0)
+vyberMena.grid(row=3,column=1,sticky=N,padx=50)
 
-napis3 = Label(hlavni,text="Zadejte castku:",font="12",)
+napis3 = Label(hlavni,text="Zadejte částku:",font="12",)
 napis3.grid(row=4,column=0,sticky=W)
 
 vstupCastka = Entry(hlavni)
-vstupCastka.grid(row=5,column=0)
+vstupCastka.grid(row=5,column=1,padx=50)
 
 def vypocet(mena,castka,operace):
     priplatek = 0.5 / 100
@@ -67,11 +67,19 @@ def aktualizaceVystupu():
     vystupniCena,vystupniPoplatek = vypocet(mena2,castka,operace2)
     vystup["text"] = "Celková částka je: {:.2f} Kč, základ je:{:.2f} a poplatek: {:.2f}".format(vystupniCena,castka,vystupniPoplatek)
 
+def aktualizaceHodin():
+    hodiny["text"]= datetime.now().strftime("%H:%M:%S")
+    hlavni.after(1000, aktualizaceHodin)
+
+hodiny = Label(hlavni)
+hodiny.grid(row=0,column=3,sticky=E)
+
+aktualizaceHodin()
+
 vystup = Label(hlavni,text=vystupText)
-vystup.grid(row=7)
+vystup.grid(row=7,column=0,columnspan=5)
 
 tlacitkoVypocet = Button(text="Vypočítat",command=aktualizaceVystupu)
-tlacitkoVypocet.grid(row=6,column=0,sticky=N)
-
+tlacitkoVypocet.grid(row=6,column=1,sticky=N)
 
 hlavni.mainloop()
