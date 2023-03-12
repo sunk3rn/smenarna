b6773ab1e1d96588f89ca18a146c5df220bc941f
sunk3rn <kajakobliha@seznam.cz>
2023-03-12 13:29:19 +0100

    created basic UI

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Vytvořte aplikaci pro obsluhu směnárny s výběrem zahraničních měn se za
 2. Pro výběr měny volíme nejlépe rozbalovací seznam (OptionMenu).
 3. V případě nákupu se zadává vstupní částka ve vybrané měně a program počítá částku v Kč, kterou má směnárna vyplatit klientovi  (od částky se odečítá poplatek 0,5%).
 4. V případě prodeje se zadává vstupní částka ve vybrané měně a program počítá částku v Kč, kterou má klient zaplatit směnárně (k částce se připočte poplatek 0,5%).
-5. V obou případech se zobrazí celková částka, základ i částka, kterou činí poplatek v korunách zaokrouhlených na desetiny Kč se zobrazením dvou desetinných míst (např. 125,20 Kč).
-6. U celkové částky by se měl objevit výrazný text, zda částka má být vyplacena nebo vybrána.
+5. V obou případech se zobrazí **celková částka, základ i částka, kterou činí poplatek** v korunách zaokrouhlených na desetiny Kč se zobrazením dvou desetinných míst (např. 125,20 Kč).
+6. U celkové částky by se měl objevit výrazný text, zda částka **má být vyplacena nebo vybrána**.
 7. Všechny vstupy je třeba ošetřit proti chybnému zadání, případně nevybrání hodnot (v případě měny a nákupu x prodeje).
 8. Aplikace bude obsahovat rovněž kurzovní lístek s minimálně 4 měnami, který obsahuje:
     - Zkratku měny 	(př. HUF)
--- smenarna.py
+++ smenarna.py
@@ -1,6 +1,50 @@
 from tkinter import *
+from datetime import datetime
 
 hlavni = Tk()
-hlavni.minsize(800,600)
+hlavni.minsize(400,300)
+hlavni.title("Směnárna")
+vyber = 0
+mena = StringVar()
+kurzovniListekOkno = Toplevel()
+kurzovniListekOkno.title("Kurzovní lístek")
+kurzovniListekOkno.minsize(300,100)
+
+napis1 = Label(hlavni,text="Vyberte akci:",font="12",)
+napis1.grid(row=0,column=0,stick=W)
+vyberKoupit = Radiobutton(hlavni,text="Koupit",variable=vyber,value=1)
+vyberProdat = Radiobutton(hlavni,text="Prodat",variable=vyber,value=2)
+vyberKoupit.grid(row=1,column=0,sticky=W)
+vyberProdat.grid(row=1,column=1,sticky=W)
+
+kurzy = {
+    "USD": 22.21,
+    "EUR": 23.67,
+    "GBP": 26.73,
+    "JPY": 0.160,
+}
+
+kurzovniListek = Text(kurzovniListekOkno,width=30,height=10)
+kurzovniListek.pack()
+for meny,kurz in kurzy.items():
+    kurzovniListek.insert(END,f"1 {meny} = {kurz} CZK\n")
+kurzovniListek.grid(row=0,column=2,rowspan=10)
+
+
+napis2 = Label(text="Vyberte měnu:",font="12",)
+napis2.grid(row=2,column=0,sticky=W)
+
+vyberMena = OptionMenu(hlavni,mena,*kurzy)
+vyberMena.grid(row=3,column=0)
+
+napis3 = Label(hlavni,text="Zadejte castku:",font="12",)
+napis3.grid(row=4,column=0,sticky=W)
+
+vstupCastka = Entry(hlavni)
+vstupCastka.grid(row=5,column=0)
+
+tlacitkoVypocet = Button(text="Vypočítat")
+tlacitkoVypocet.grid(row=6,column=0,sticky=W)
+
 
 hlavni.mainloop()
